124778a6cd8bc8bd03fc5eda5080af13a2c7feda
Guillermo Esteves <gfesteves@gmail.com>
2014-04-29 09:57:08 -0400

    Add build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,5 +52,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('default', ['watch']);
-
+  grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
 };
