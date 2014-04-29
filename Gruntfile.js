5b8b7ceac6578b6535f8b19df0a669ecb244a981
Guillermo Esteves <gfesteves@gmail.com>
2014-04-29 09:58:19 -0400

    Don't make watch the default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default', ['watch']);
-  grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
