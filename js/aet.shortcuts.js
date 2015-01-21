46dbf5ec05aadac7918133e4c7a0bebead78fa82
Guillermo Esteves <g@gesteves.com>
2015-01-20 20:18:52 -0500

    Add random keyboard shortcut

--- js/aet.shortcuts.js
+++ js/aet.shortcuts.js
@@ -18,32 +18,44 @@ AET.Shortcuts = (function ($) {
   var navigate = function (e) {
     var key = e.keyCode || e.which;
     var keys = {
-      left: 37,
-      right: 39,
-      up: 38,
-      down: 40,
-      j: 74,
-      k: 75
+      left  : 37,
+      right : 39,
+      up    : 38,
+      down  : 40,
+      j     : 74,
+      k     : 75,
+      r     : 82
     };
     switch (key) {
       case keys.left:
+      case keys.j:
         newerPage();
         break;
       case keys.right:
+      case keys.k:
         olderPage();
+        break;
+      case keys.r:
+        randomPost();
         break;
     }
   };
 
   var newerPage = function () {
-    if (opts.newer_page.length) {
+    if (opts.newer_page.length && !$('input, textarea').is(':focus')) {
       window.location.href = opts.newer_page.attr('href');
     }
   };
 
   var olderPage = function () {
-    if (opts.older_page.length) {
+    if (opts.older_page.length && !$('input, textarea').is(':focus')) {
       window.location.href = opts.older_page.attr('href');
+    }
+  };
+
+  var randomPost = function () {
+    if (!$('input, textarea').is(':focus')) {
+      window.location.href = window.location.origin + '/random';
     }
   };
 
